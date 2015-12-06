9446b612e2ff9d6e6f609c6d32357d9a454b2cc8
Mike Maelzer <mmaelzer@gmail.com>
2015-12-06 14:16:29 -0800

    Add a MotionWithStep to provide a way to reduce the CPU burden of motion detection at the cost of precision.

--- camotion.go
+++ camotion.go
@@ -13,22 +13,24 @@ func pixels(img image.Image) int {
 	return b.Dx() * b.Dy()
 }
 
-// Motion will determine if motion occurs between two images
-// given a minChange value which is a 1-100 value representing
-// percentage of image changed and a threshold value which
-// sets a below this value is noise. A reasonable threshold default for
-// jpegs (and potentially other formats) is 2500.
-func Motion(img1, img2 image.Image, minChange, threshold int) bool {
+// MotionWithStep will call Motion() with a step value that determines
+// how many pixels to skip between comparisons. By default, Motion has step
+// of 1, which means that every pixel will be analyzed to determine if
+// motion is detected. Adding a step value provides a way to dial down
+// processing requirements in a linear fashion to reduce the CPU burden
+// of motion detection at the cost of precision.
+func MotionWithStep(img1, img2 image.Image, minChange, threshold, step int) bool {
 	if !img1.Bounds().Eq(img2.Bounds()) {
 		return false
 	}
 
-	mc := float64(pixels(img1)) * float64(float64(minChange)/100.0)
+	psize := float64(pixels(img1))
+	mc := (psize / float64(step*step)) * float64(float64(minChange)/100.0)
 	change := float64(0)
 
 	b := img1.Bounds()
-	for x := b.Min.X; x <= b.Max.X; x++ {
-		for y := b.Min.Y; y <= b.Max.Y; y++ {
+	for x := b.Min.X; x <= b.Max.X; x += step {
+		for y := b.Min.Y; y <= b.Max.Y; y += step {
 			avg1 := average(img1.At(x, y))
 			avg2 := average(img2.At(x, y))
 			diff := math.Abs(avg1 - avg2)
@@ -41,6 +43,15 @@ func Motion(img1, img2 image.Image, minChange, threshold int) bool {
 		}
 	}
 	return false
+}
+
+// Motion will determine if motion occurs between two images
+// given a minChange value which is a 1-100 value representing
+// percentage of image changed and a threshold value which
+// sets a below this value is noise. A reasonable threshold default for
+// jpegs (and potentially other formats) is 2500.
+func Motion(img1, img2 image.Image, minChange, threshold int) bool {
+	return MotionWithStep(img1, img2, minChange, threshold, 1)
 }
 
 // Returns a black and white image where white pixels denote a change
